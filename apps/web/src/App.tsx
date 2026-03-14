b1e25999ed41c9f7951f81599a31a0c87f00a105
corentin <leotardcorentin@gmail.com>
2026-03-14 20:06:40 +0100

    1- wip

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -30,7 +30,7 @@ export function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <PwaProvider>
-      <Toaster position="top-center" richColors closeButton />
+      <Toaster position="top-center" richColors closeButton offset="env(safe-area-inset-top, 14px)" />
       <BrowserRouter>
         <AuthProvider>
           <Routes>
